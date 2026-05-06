f575127c90903baa6ddc238028a5832a7e840f6a
Lukas Najman <lukas.najman@cloudfield.cz>
2026-05-06 13:14:15 +0200

    Introduce configurable authentication cache properties with TTL and max size

--- src/main/java/com/czertainly/core/config/CacheConfig.java
+++ src/main/java/com/czertainly/core/config/CacheConfig.java
@@ -3,6 +3,7 @@ package com.czertainly.core.config;
 import com.czertainly.core.security.authn.client.TokenJtiIndex;
 import com.czertainly.core.security.authn.client.UserCertificateIndex;
 import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
@@ -14,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching(order = Ordered.HIGHEST_PRECEDENCE)
+@EnableConfigurationProperties(AuthCacheProperties.class)
 public class CacheConfig {
 
     public static final String SIGNING_PROFILES_CACHE = "signingProfiles";
@@ -27,19 +29,19 @@ public class CacheConfig {
     public static final String CRYPTOGRAPHIC_KEY_ITEM_CACHE = "cryptographicKeyItem";
 
     @Bean
-    public CacheManager cacheManager(TokenJtiIndex tokenJtiIndex, UserCertificateIndex userCertificateIndex) {
+    public CacheManager cacheManager(AuthCacheProperties cacheProperties, TokenJtiIndex tokenJtiIndex, UserCertificateIndex userCertificateIndex) {
         CaffeineCacheManager mgr = new CaffeineCacheManager(SIGNING_PROFILES_CACHE, TSP_PROFILES_CACHE, CERTIFICATE_CHAIN_CACHE, SYSTEM_USER_AUTH_CACHE, USER_UUID_AUTH_CACHE, FORMATTER_CONNECTOR_CACHE, CRYPTOGRAPHIC_KEY_ITEM_CACHE);
         mgr.setCaffeine(Caffeine.newBuilder()
-                .expireAfterWrite(5, TimeUnit.MINUTES)
-                .maximumSize(500));
+                .expireAfterWrite(cacheProperties.ttlMinutes(), TimeUnit.MINUTES)
+                .maximumSize(cacheProperties.maxSize()));
         mgr.registerCustomCache(CERTIFICATE_AUTH_CACHE, Caffeine.newBuilder()
-                .expireAfterWrite(5, TimeUnit.MINUTES)
-                .maximumSize(500)
+                .expireAfterWrite(cacheProperties.ttlMinutes(), TimeUnit.MINUTES)
+                .maximumSize(cacheProperties.maxSize())
                 .removalListener(userCertificateIndex)
                 .build());
         mgr.registerCustomCache(TOKEN_AUTH_CACHE, Caffeine.newBuilder()
-                .expireAfterWrite(5, TimeUnit.MINUTES)
-                .maximumSize(500)
+                .expireAfterWrite(cacheProperties.ttlMinutes(), TimeUnit.MINUTES)
+                .maximumSize(cacheProperties.maxSize())
                 .removalListener(tokenJtiIndex)
                 .build());
         return mgr;
